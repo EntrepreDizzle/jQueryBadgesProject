d7318b05d8cc6289e8cb6a30ad96a7c8d618bc08
EntrepreDizzle <danreeves74@outlook.com>
2017-10-15 14:35:20 -0700

    Finalize steps 6, 7, 8 and 9 in main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,9 +2,18 @@ $(function () {
 
   $.ajax({
     url: 'https://www.codeschool.com/users/danreeves74.json',
-    dataType: jsonp,
+    dataType: "jsonp",
     success: function (response) {
-      console.log(response);
+      var elements = [];
+      for (var i = 0; i < response.courses.completed.length; i++) {
+        var course = response.courses.completed[i];
+        var pic = course.badge;
+        var title = course.title;
+        var url = course.url;
+        var element = "<div class='course'><h3 class='title'>"+title+"</h3><img src='"+pic+"'/><a href='"+url+"' target='_blank' class='btn btn-primary'>See Course</a></div>";
+        elements.push(element);
+      }
+      $('#badges').append(elements);
     }
   });
 
